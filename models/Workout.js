1d15b73576907a287034e54ddee62e2a5bc5728b
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-11 19:37:51 -0500

    wourkout model

---
+++ models/Workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        { 
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number,
+                required: true
+            },
+            reps: {
+                type: Number,
+                required: true
+            },
+            sets: {
+                type: Number,
+                required: true
+            },
+        },
+    ]
+})
