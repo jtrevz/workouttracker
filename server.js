1ccc24110fa2a1f2c8e113200550b8792d383165
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-05-11 20:22:39 -0500

    models fixed to link directly

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
